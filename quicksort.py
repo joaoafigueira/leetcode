1cff24bc3397e6396fd096e0f8ec86fed75c0990
João Victor <79715612+joaoafigueira@users.noreply.github.com>
2023-04-02 10:26:28 -0300

    style: removing comments

--- quicksort.py
+++ quicksort.py
@@ -1,14 +1,14 @@
 
 # Quicksort it is a sorting algorithm, that uses the divide and conquering thinking and 
 # it is much faster than selecion sort, where the big O grows accordingly the pivot.
-# Below we have an example of the algorithm
+# Below we have an example of the algorithm.
 
 
 def quicksort(arr):
     if len(arr) < 2:
         return arr
     else:
-        pivot = arr[0] # 2 4
+        pivot = arr[0]
         minors = [i for i in arr[1:] if i <= pivot] 
         bigger = [i for i in arr[1:] if i > pivot]   
         return quicksort(minors) + [pivot] + quicksort(bigger)
